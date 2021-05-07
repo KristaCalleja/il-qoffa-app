11225dec93191d05763b1e1d86a1e842e8fc1238
KristaCalleja <kristacm@gmail.com>
2021-05-07 16:06:43 -0300

    Add POST action

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,11 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
+  get 'tasks', to: 'tasks#index'
+  # For the CREATE route, we need to do a NEW route, which needs to come before SHOW
+  get 'tasks/new', to: 'tasks#new'
+  get 'tasks/:id', to: 'tasks#show', as: :task
+
+  # POST the form
+  post 'tasks', to: 'tasks#create'
 end

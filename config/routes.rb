ee670cfbabf4c63b7fe1ba1ec43341e4fb080fee
KristaCalleja <kristacm@gmail.com>
2021-05-11 16:53:49 -0300

    Add custom edit route using 'as' specifier

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,27 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'pages#home'
+  # PAGES GETS
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
+
+
   # READ ALL
   get 'tasks', to: 'tasks#index'
-  # For the CREATE route, we need to do a NEW route, which needs to come before SHOW
-  get 'tasks/new', to: 'tasks#new'
-  # READ ONE
-  get 'tasks/:id', to: 'tasks#show', as: :task
   # POST the form
   post 'tasks', to: 'tasks#create'
+
+  # For the CREATE route, we need to do a NEW route, which needs to come before SHOW
+  get 'tasks/new', to: 'tasks#new'
+  # GET with pre-filled attributes for editing
+  get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+
+  # READ ONE
+  get 'tasks/:id', to: 'tasks#show', as: :task
+
+  # PATCH the form
+  patch 'tasks/:id', to: 'tasks#update'
+  
   # DELETE
   delete 'tasks/:id', to: 'tasks#destroy'
 end
